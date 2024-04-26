b8f2fc15e12c102aca27ec2d200a407eae1c18b6
filmtree <filmtree@naver.com>
2024-04-26 23:01:15 +0900

    1

---
+++ src/layout/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <header>
+      <Header />
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <Footer>
+        <template #footer>Footer</template>
+      </Footer>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+</script>
