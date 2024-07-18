25cab58aa610ad39089d0d4c1c7be7590fd68497
filmtree <filmtree@naver.com>
2024-07-18 22:14:06 +0900

    07183

--- src/views/TooltipMessage.vue
+++ src/views/TooltipMessage.vue
@@ -1,29 +1,38 @@
 <template>
-  <div>
-    <button v-tooltip="'You have new messages1.'">Test1</button>
-    <button v-tooltip="{ content: '<b>Bold</b>', html: true }">HTML</button>
-    <button v-tooltip.bottom-start="'You have new messages3.'">Position</button>
-    <button v-tooltip.right-end="'You have new messages4.'">Position</button>
-    <button v-tooltip="'You have new messages5.'">Test5</button>
-    <button v-tooltip="'You have new messages6.'">Test6</button>
-    <br />
-    <br />
-    <VTooltip>
-      <a>Sponsor me</a>
+  <div class="container">
 
-      <template #popper> Help me fund my Open Source work! </template>
-    </VTooltip>
-    <br />
-    <VDropdown :distance="6" :triggers="['hover', 'focus']" placement="bottom-start">
-      <button>Hover me Dropdown</button>
-      <template #popper>
-        <input class="tooltip-content" v-model="msg" placeholder="Tooltip content" />
-        <p>{{ msg }}</p>
-      </template>
-    </VDropdown>
+    <div>
+      <button v-tooltip="'You have new messages1.'">Test1</button>
+      <button v-tooltip="{ content: '<b>Bold</b>', html: true }">HTML</button>
+      <button v-tooltip.bottom-start="'You have new messages3.'">Position</button>
+      <button v-tooltip.right-end="'You have new messages4.'">Position</button>
+      <button v-tooltip="'You have new messages5.'">Test5</button>
+      <button v-tooltip="'You have new messages6.'">Test6</button>
+    </div>
 
-    <div class="flex gap-4">
-      <VDropdown placement="bottom-start">
+    <br />
+
+    <div class="tooltip-wrap">
+      <VTooltip placement="bottom-start">
+        <a>Sponsor me</a>
+        <template #popper> Help me fund my Open Source work! </template>
+      </VTooltip>
+    </div>
+
+    <br />
+
+    <div class="tooltip-wrap">
+      <VDropdown :distance="6" :triggers="['hover', 'focus']" placement="bottom-start">
+        <button>Hover me Dropdown</button>
+        <template #popper>
+          <input class="tooltip-content" v-model="msg" placeholder="Tooltip content" />
+          <p>{{ msg }}</p>
+        </template>
+      </VDropdown>
+    </div>
+
+    <div class="tooltip-wrap">
+      <VDropdown placement="bottom">
         <button>Click me Dropdown</button>
         <template #popper>
           <div class="p-5">
@@ -38,6 +47,7 @@
       </VDropdown>
       <button id="noop-btn">I do nothing.</button>
     </div>
+
   </div>
 </template>
 
@@ -52,7 +62,23 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  gap: 20px;
+}
+
+.tooltip-wrap {
+  display: flex;
+  flex-direction: column;  
+  align-items: center;  
+  gap: 10px;
+}
+
+
 button {
   padding: 8px 16px;
 }
+
 </style>
