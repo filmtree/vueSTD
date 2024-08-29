9c063bbb50f0d6decd5e78c37295df06362c7242
filmtree <filmtree@naver.com>
2024-08-29 22:42:59 +0900

    082906

--- src/basic/09vueTransitionHook.vue
+++ src/basic/09vueTransitionHook.vue
@@ -15,45 +15,54 @@
 
 <script setup>
 function onBeforeEnter(el) {
+  el.style.transition = 'height .3s ease'
   el.style.height = 0
 }
+
+// el.offsetHeight를 호출함으로써 브라우저는 이전 스타일을 적용한 후,
+// 다음 스타일을 적용하기 전에 레이아웃을 다시 계산하게 됩니다.
+// 이렇게 하면 브라우저는 두 상태(높이 0과 실제 높이) 간의 변화를 인식하고,
+// CSS 전환 효과를 적용할 수 있습니다.
 
 function onEnter(el, done) {
   el.offsetHeight // trigger reflow
   el.style.height = el.scrollHeight + 'px'
-  done()
+  el.addEventListener('transitionend', done)
 }
 
-function onAfterEnter(el) {}
+// function onAfterEnter(el) {}
 
-function onEnterCancelled(el) {}
+// function onEnterCancelled(el) {}
 
 function onBeforeLeave(el) {
+  el.style.transition = 'height .3s ease'
   el.style.height = el.scrollHeight + 'px'
 }
 
 function onLeave(el, done) {
   el.style.height = 0
-  done()
+  el.addEventListener('transitionend', done)
 }
 
-function onAfterLeave(el) {}
+// function onAfterLeave(el) {}
 
-function onLeaveCancelled(el) {}
+// function onLeaveCancelled(el) {}
 </script>
 
 <style>
 .container {
-  overflow: hidden; /* 내용이 잘리지 않도록 overflow hidden 추가 */
+  overflow: hidden; 
 }
 
-.transition-enter-active,
-.transition-leave-active {
+/* 
+
+.transition-enter-active, .transition-leave-active {
   transition: height 0.3s ease-in-out;
 }
 
-.transition-enter,
-.transition-leave-to {
-  height: 0; /* enter 시 초기 높이와 leave 시 최종 높이 */
-}
+.transition-enter, .transition-leave-to {
+  height: 0; 
+} 
+
+*/
 </style>
