5c416569de2af2a62f4d3ca8f0442b48058697d5
filmtree <filmtree@naver.com>
2024-09-06 02:33:06 +0900

    090601

--- src/composable/useScroll.ts
+++ src/composable/useScroll.ts
@@ -1,9 +1,9 @@
-// useScroll.js
-import { ref, onMounted, onUnmounted } from 'vue';
+// useScroll.ts
+import { ref, onMounted, onUnmounted, Ref } from 'vue';
 
-export function useScroll(scrollElement) {
-  const scrollY = ref(0);
-  const elHeight = ref(0); // 헤더 높이를 저장할 변수
+export function useScroll(scrollElement: Ref<HTMLElement | null>) {
+  const scrollY = ref<number>(0);
+  const elHeight = ref<number>(0); // 헤더 높이를 저장할 변수
 
   const handleScroll = () => {
     scrollY.value = Math.min(window.scrollY, elHeight.value); // 헤더 높이 이상으로 스크롤이 가지 않도록 제한
